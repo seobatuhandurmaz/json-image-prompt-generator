fc86fa38af358d18ff0ca5073bfdb6887ae40e4a
Batuhan Durmaz <batuhanndurmazz33@gmail.com>
2025-08-16 12:36:48 +0300

    Delete backend/app directory

--- backend/app/utils.py
+++
@@ -1,6 +0,0 @@
-def hex_to_rgba(hex_str: str, alpha: float) -> str:
-    hex_str = hex_str.lstrip("#")
-    r = int(hex_str[0:2], 16)
-    g = int(hex_str[2:4], 16)
-    b = int(hex_str[4:6], 16)
-    return f"rgba({r},{g},{b},{alpha:.2f})"
